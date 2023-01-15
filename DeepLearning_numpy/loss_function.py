27c70b30d2a1e922c9bb935f74937a09ed486633
dongyyyyy <dongyoung0218@gmail.com>
2023-01-15 22:47:16 +0900

    update

--- DeepLearning_numpy/loss_function.py
+++ DeepLearning_numpy/loss_function.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def one_hot(y, length_of_onehot):
-    return np.eye(length_of_onehot)[y].reshape(-1, length_of_onehot)
+    ###########################################################
 
 
 def crossentropy(z, y):  # sample의 평균 // 클래스간의 합
-    return np.mean(-1 * np.sum((one_hot(y,10) * np.log(z)), axis=1))
+    ###########################################################
 
 
 def ave_accuracy(output, y):
-    return np.mean(np.argmax(output, axis=1).reshape(-1, 1) == y, axis=0)
+    ###########################################################
--- DeepLearning_numpy/model.py
+++ DeepLearning_numpy/model.py
@@ -2,17 +2,15 @@ import numpy as np
 import loss_function as loss 
 
 def ReLU(x):
-    return np.maximum(0, x)
-    #return np.fmax(0, x)
+    ###########################################################
 
 
 def dReLU(x):
-    # relu가 x >= 0 일때, y=x이면 아래와 같다.
-    return np.where(x >= 0, 1, 0)
+    ###########################################################
 
 
 def softmax(z):
-    return np.exp(z) / np.sum(np.exp(z), axis=1).reshape(-1, 1)
+    ###########################################################
 
 
 class Network():
@@ -29,27 +27,16 @@ class Network():
         self.W = dict()
         self.b = dict()
         # update로 모두 처리
-        for i in range(1, self.num_layers):  # 1에서부터 시작
-            self.W.update({i: init_coef * np.random.randn(self.size_of_input, hidden_layers[i - 1])})
-            self.b.update({i: init_coef * np.random.randn(1, hidden_layers[i - 1])})
-            self.size_of_input = hidden_layers[i - 1]
-
-        self.W.update({self.num_layers: init_coef * np.random.randn(self.size_of_input, output_size)})
-        self.b.update({self.num_layers: init_coef * np.random.randn(1, output_size)})
+        ###########################################################
+        ###########################################################
 
     def forward(self, x):  # x shape = batch X input_size
         ############### Write code ######################
         self.Z = dict()  # linear regression dict initialization
         self.A = dict()  # activation function dict initialization
 
-        for i in range(1, self.num_layers + 1):
-            x = np.matmul(x, self.W[i]) + self.b[i]
-            self.Z.update({i: np.copy(x)})
-            if i < self.num_layers:  # Output layer 의 경우 activation function 을 쓰지 않기 때문에
-                x = ReLU(x)
-                self.A.update({i: np.copy(x)})
-        x = softmax(x)
-        self.A.update({self.num_layers: x})
+        ###########################################################
+        ###########################################################
 
         return x
 
@@ -62,22 +49,10 @@ class Network():
 
         # Calculate Gradients
 
-        self.delta = {self.num_layers: self.A[self.num_layers] - loss.one_hot(y, self.output_size)}
-        self.dW = {self.num_layers: np.matmul(self.A[self.num_layers - 1].T,
-                                              self.delta[self.num_layers]) / batch_size}
-        self.db = {self.num_layers: np.matmul(np.ones((1, batch_size)), self.delta[self.num_layers]) / batch_size}
-
-        for i in range(self.num_layers - 1, 1, -1):
-            self.delta.update({i: np.matmul(self.delta[i + 1], self.W[i + 1].T) * dReLU(self.Z[i])})
-
-            self.dW.update({i: np.matmul(self.A[i - 1].T, self.delta[i]) / batch_size})
-            self.db.update({i: np.matmul(np.ones((1, batch_size)), self.delta[i]) / batch_size})
-
-        self.delta.update({1: np.matmul(self.delta[2], self.W[2].T) * dReLU(self.Z[1])})
-        self.dW.update({1: np.matmul(X.T, self.delta[1]) / batch_size})
-        self.db.update({1: np.matmul(np.ones((1, batch_size)), self.delta[1]) / batch_size})
+        ###########################################################
+        ###########################################################
 
     def update(self, alpha):
-        for i in range(1, self.num_layers + 1):
-            self.W[i] = self.W[i] - alpha * (self.dW[i])
-            self.b[i] = self.b[i] - alpha * (self.db[i])
+        ###########################################################
+        
+        ###########################################################
